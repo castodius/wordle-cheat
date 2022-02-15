889fed6dba2d7146112bdd0b646c9165e356ed03
Urban Pettersson <urbanpe88@gmail.com>
2022-02-15 20:50:50 +0100

    feat: add support for function import and executable

---
+++ src/bin.js
@@ -0,0 +1,4 @@
+const { getAnswer } = require("./index")
+
+const arg = process.argv[2]
+getAnswer(arg)
--- src/index.js
+++ src/index.js
@@ -2,34 +2,41 @@ const { differenceInDays, parse, add, format } = require("date-fns")
 
 const { answers } = require('./answers')
 
-const defaultDatePattern = /^\d{4}-\d{2}-\d{2}$/
+const dateRegexp = /^\d{4}-\d{2}-\d{2}$/
+const dateFormat = 'yyyy-MM-dd'
+const startDateString = '2021-06-19'
+const startDate = parse(startDateString, dateFormat, new Date())
 
 const logAndExit = (message) => {
   console.log(message)
   process.exit()
 }
 
-const getCheatDate = () => {
-  const argDate = process.argv[2]
-  if (!argDate) {
+const getCheatDate = (dateString) => {
+  if (!dateString) {
     return new Date()
   }
-  if (!defaultDatePattern.test(argDate)) {
-    logAndExit(`Argument "${argDate}" does not match pattern yyyy-MM-dd`)
-  }
 
-  return parse(argDate, 'yyyy-MM-dd', new Date())
+  if (!dateRegexp.test(dateString)) {
+    logAndExit(`Argument "${dateString}" does not match pattern ${dateFormat}`)
+  }
+  
+  return parse(dateString, dateFormat, new Date())
 }
 
-const startDateString = '2021-06-19'
-const startDate = parse(startDateString, 'yyyy-MM-dd', new Date())
-const cheatDate = getCheatDate()
+const getAnswer = (arg) => {
+  const cheatDate = getCheatDate(arg)
+  const diff = differenceInDays(cheatDate, startDate)
 
-const diff = differenceInDays(cheatDate, startDate)
-if (diff < 0) {
-  logAndExit(`Your chosen date is too far into the past. The earliest available date is ${startDateString}.`)
-} else if (diff >= answers.length) {
-  console.log(`Your chosen date is too far into the future. The latest date available date is ${format(add(startDate, { days: answers.length - 1 }), 'yyyy-MM-dd')}`)
-} else {
-  console.log(answers[diff])
+  if (diff < 0) {
+    console.log(`Your chosen date is too far into the past. The earliest available date is ${startDateString}.`)
+  } else if (diff >= answers.length) {
+    console.log(`Your chosen date is too far into the future. The latest date available date is ${format(add(startDate, { days: answers.length - 1 }), dateFormat)}`)
+  } else {
+    console.log(answers[diff])
+  }
+}
+
+module.exports = {
+  getAnswer
 }
