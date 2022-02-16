dae8e7f184588f0d4dd76ac3fe2feab3788aadc8
Urban Pettersson <urbanpe88@gmail.com>
2022-02-16 09:44:13 +0100

    chore: add types for getAnswer

---
+++ src/index.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Return the daily wordle answer
+ * @param dateString 
+ * Optional date string of format 'yyyy-MM-dd' to get word for. Defaults to today
+ * @returns
+ * The daily wordle answer
+ * @throws
+ * If the supplied string is defined and not of the proper format
+ * @throws
+ * If date is before the earliest possible date
+ * @throws
+ * If date is after the latest possible date
+ */
+export const getAnswer = (dateString?: string) => string
